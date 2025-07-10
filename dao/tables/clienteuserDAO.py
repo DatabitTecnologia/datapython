1c82dae7844689a3037069312325aafd13dd453c
Sidney <sistemas@databit.com.br>
2025-07-10 17:09:43 -0300

    Tarefas 10/07/2025

---
+++ dao/tables/clienteuserDAO.py
@@ -0,0 +1,129 @@
+﻿"""
+File : clienteuserDAO.py
+
+Function : Metodos de manuseio de registros no banco de dados para a tabela de Definições de Usuarios (DataService) (TB01066)
+
+Author: Sidney Sanches Moreira
+
+Object DataBase: Table TB01066
+"""
+import dao
+import log
+
+
+class ClienteUserDAO(dao.Dao):
+
+    def __init__(self):
+        super().__init__("TB01066", "Usuário", "o", "Definições de Usuarios (DataService)", "N", "N", "N", "TB01066")
+
+    def listClienteUser(self, params):
+        """
+        Metodo de listagem de clienteuser tendo o título das colunas como "atributos da classe"
+
+        token: Token de conexao ao banco de dados (Obrigatorio)
+
+        filtro: Qual e o criterio de filtro que esta sendo feita a listagem, podendo tambem, colocar o tanto "order by"
+        quanto "group by" neste parametro (Opcional)
+
+        campos: Quais seriam os campos (colunas) que desejam ser retornados na pesquisa (Opcional)
+
+        :param params: JSON com as informacoes de token, filtro, campos (colunas)
+
+        :return: JSON
+        """
+        try:
+            return super().listagem(params, 0)
+        except Exception as erro:
+            return log.geralog(-1, "ERRO", f"{erro=}")
+
+    def fieldsClienteUser(self, params):
+        """
+        Metodo de listagem de clienteuser tendo o titulo das colunas como "nome dos campos"
+
+        token: Token de conexao ao banco de dados (Obrigatório)
+
+        filtro: Qual e o critério de filtro que esta sendo feita a listagem, podendo tambem, colocar o tanto 'order by',
+        quanto 'group by', neste parametro (Opcional)
+
+        campos: Quais seriam os campos (colunas) que desejam ser retornados na pesquisa (Opcional)
+
+        :param params: JSON com as informacoes de token, filtro, campos (colunas)
+
+        :return: JSON
+        """
+        try:
+            return super().listagem(params, 1)
+        except Exception as erro:
+            return log.geralog(-1, "ERRO", f"{erro=}")
+
+    def dictClienteUser(self, params):
+        """
+        Metodo de listagem de clienteuser tendo o titulo das colunas como "descrição dos campos"
+
+        token: Token de conexão ao banco de dados (Obrigatório)
+
+        filtro: Qual e o critério de filtro que esta sendo feita a listagem, podendo tambem, colocar o tanto 'order by',
+        quanto 'group by', neste parametro (Opcional)
+
+        campos: Quais seriam os campos (colunas) que desejam ser retornados na pesquisa (Opcional)
+
+        :param params: JSON com as informacoes de token, filtro, campos (colunas)
+
+        :return: JSON
+        """
+        try:
+            return super().listagem(params, 2)
+        except Exception as erro:
+            return log.geralog(-1, "ERRO", f"{erro=}")
+
+    def findClienteUser(self, params):
+        """
+        Metodo para que procurar um clienteuser em específico
+
+        :param params: JSON - Onde precisa ser passados os seguintes valores:
+
+        token: Token da Databit (Obrigatório)
+
+        filtro: Filtro desejaddo (Opcional)
+
+        ordem: Ordem do resultado da pesquisa (Opcional)
+
+        :return: JSON -> ClienteUser
+        """
+        try:
+            return super().find(params)
+        except Exception as erro:
+            return log.geralog(-1, "ERRO", f"{erro=}")
+
+    def insClienteUser(self, params):
+        """
+        Metodo onde insere ClienteUser no banco de dados.
+        :param params: JSON - Valores
+        :return: JSON
+        """
+        try:
+            return super().insert(params)
+        except Exception as erro:
+            return log.geralog(-1, "ERRO", f"{erro=}")
+
+    def updClienteUser(self, params):
+        """
+        Metodo onde altera ClienteUser no banco de dados.
+        :param params: JSON - Valores
+        :return: JSON
+        """
+        try:
+            return super().update(params)
+        except Exception as erro:
+            return log.geralog(-1, "ERRO", f"{erro=}")
+
+    def delClienteUser(self, params):
+        """
+        Metodo onde exclui ClienteUser no banco de dados.
+        :param params: JSON - Valores
+        :return: JSON
+        """
+        try:
+            return super().delete(params)
+        except Exception as erro:
+            return log.geralog(-1, "ERRO", f"{erro=}")
